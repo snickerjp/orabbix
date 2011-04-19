f1309cfe6a745903e3802321f4ea07ba62eaea85
rootzone <rootzone@abe186de-f045-48bc-9617-b32b725a8f55>
2011-04-19 20:05:17 +0000

    git-svn-id: https://svn.code.sf.net/p/orabbix/code/src@375 abe186de-f045-48bc-9617-b32b725a8f55

--- main.java
+++ main.java
@@ -1,4 +1,7 @@
-/* This file is part of orabbix.
+/*
+ * Copyright (C) 2010 Andrea Dalle Vacche.
+ * 
+ * This file is part of orabbix.
  *
  * orabbix is free software: you can redistribute it and/or modify it under the
  * terms of the GNU General Public License as published by the Free Software
@@ -11,20 +14,15 @@
  * details.
  * 
  * You should have received a copy of the GNU General Public License along with
- * orabbix. If not, see <http://www.gnu.org/licenses/>.
+ *  . If not, see <http://www.gnu.org/licenses/>.
  */
 
 import java.lang.management.ManagementFactory;
 import java.lang.management.RuntimeMXBean;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.util.Hashtable;
 import java.util.Locale;
-import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.LinkedBlockingQueue;
 
 import org.apache.commons.dbcp.datasources.SharedPoolDataSource;
 import org.apache.log4j.Level;
@@ -35,14 +33,10 @@ import com.smartmarmot.orabbix.DBConn;
 import com.smartmarmot.orabbix.DBJob;
 import com.smartmarmot.orabbix.Query;
 import com.smartmarmot.orabbix.Querybox;
-import com.smartmarmot.orabbix.Sender;
 import com.smartmarmot.orabbix.Utility;
-import com.smartmarmot.orabbix.ZabbixItem;
 
 public class main {
-	public static final String Version = "Version 1.1.0";
-	public static final String Banner = Constants.PROJECT_NAME + " " + Version;
-
+	
 	/**
 	 * @param args
 	 * @throws Exception
@@ -54,7 +48,7 @@ public class main {
 				printUsage();
 				System.exit(0);
 			}
-			Configurator.logThis(Level.INFO, "Starting " + Banner);
+			Configurator.logThis(Level.INFO, "Starting " + Constants.BANNER);
 
 			String configFile;
 			configFile = new String(args[0].toString());
@@ -63,11 +57,9 @@ public class main {
 			
 			RuntimeMXBean rmxb = ManagementFactory.getRuntimeMXBean();
 			String pid = rmxb.getName();
-			Configurator.logThis(Level.INFO, "Orabbix started with pid:"
-					+ pid.split("@")[0].toString());
-
+			Configurator.logThis(Level.INFO, Constants.PROJECT_NAME
+					+ " started with pid:" + pid.split("@")[0].toString());
 			// System.out.print("pid: "+pid.split("@")[0].toString());
-
 			String pidfile = cfg.getPidFile();
 			try {
 				Utility.writePid(pid.split("@")[0].toString(), pidfile);
@@ -78,7 +70,13 @@ public class main {
 
 			Locale.setDefault(Locale.US);
 			DBConn[] myDBConn = cfg.getConnections();
-
+			if (myDBConn == null) {
+				Configurator.logThis(Level.ERROR,
+						"ERROR on main - Connections is empty or null");
+			} else if (myDBConn.length == 0) {
+				Configurator.logThis(Level.ERROR,
+						"ERROR on main - Connections is empty or null");
+			}
 			/**
 			 * retrieve maxThread
 			 */
@@ -90,15 +88,14 @@ public class main {
 						"MaxThread not defined calculated maxThread = "
 								+ myDBConn.length * 3);
 			}
+			if (maxThread == null)
+				maxThread = 0;
 			if (maxThread == 0) {
 				maxThread = myDBConn.length * 3;
 			}
 			
 			ExecutorService executor = Executors.newFixedThreadPool(maxThread
 					.intValue());
-
-			
-
 			/**
 			 * populate qbox
 			 */
@@ -127,7 +124,9 @@ public class main {
 				 */
 				Configurator c = new Configurator(configFile);
 
-				
+				/*
+				 * here i rebuild DB's List
+				 * */
 				if (!c.isEqualsDBList(myDBConn)) {
 					
 					 // rebuild connections DBConn[]
@@ -151,96 +150,29 @@ public class main {
 				}
 
 			
-
+				/*
+				 * ready to run query
+				 * 
+				 * */
+				
 				for (int i = 0; i < myDBConn.length; i++) {
 					Querybox actqb = qbox.get(myDBConn[i].getName());
 					actqb.refresh();
 					Query[] q = actqb.getQueries();
 
 					SharedPoolDataSource spds = myDBConn[i].getSPDS();
-
-					Configurator.logThis(Level.DEBUG,
-							"Retrieve connection for dbname ->" + myDBConn[i].getName());
-					boolean alive = false;
+					
 					Hashtable<String, Integer> zabbixServers = c
 							.getZabbixServers();
-
-					try {
-						Connection cn = spds.getConnection();
-						PreparedStatement p_stmt = null;
-						p_stmt = cn
-								.prepareStatement("select sysdate from dual");
-						ResultSet rs = null;
-						rs = p_stmt.executeQuery();
-						rs.next();
-						cn.close();
-						cn = null;
-						BlockingQueue<ZabbixItem> _queue = new LinkedBlockingQueue<ZabbixItem>();
-						_queue.offer(new ZabbixItem("alive", "1"));
-						_queue.offer(new ZabbixItem("OrabbixVersion", Banner));
-						Sender sender = new Sender(_queue, zabbixServers,
-								myDBConn[i].getName());
-						sender.run();
-						alive = true;
-						Configurator.logThis(Level.DEBUG, "Database "
-								+ myDBConn[i].getName() + " is alive");
-					} catch (Exception ex) {
-						BlockingQueue<ZabbixItem> _queue = new LinkedBlockingQueue<ZabbixItem>();
-						_queue.offer(new ZabbixItem("alive", "0"));
-						Sender sender = new Sender(_queue, zabbixServers,
-								myDBConn[i].getName());
-						sender.run();
-						Configurator.logThis(Level.ERROR,
-								"Error diuring alive testing for dbname ->"
-										+ myDBConn[i].getName() + ex);
-					}
-
-					if (alive) {
-						// System.out.println("retrieve connection for dbname ->"+newDBList[i]);
-						try {
-							Locale.setDefault(Locale.US);
-							Connection con = spds.getConnection();
-							Configurator.logThis(Level.DEBUG,
-									"sharedpooldatasource idle connection -->"
-											+ spds.getNumIdle()
-											+ " active connetion -->"
-											+ spds.getNumActive() + ""
-											+ " dbname -->" + myDBConn[i].getName());
-							Configurator
-									.logThis(Level.DEBUG,
-											"Starting ZabbixTrapper for "
-													+ myDBConn[i].getName());
-							// final ZabbixTrapper trapper =
-							// c.getTrapper(newDBList[i]);
-							Runnable runner = new DBJob(con, q,
-									Constants.QUERY_LIST, zabbixServers,
-									myDBConn[i].getName());
-							executor.execute(runner);
-
-						} catch (Exception e) {
-
-							Configurator.logThis(Level.ERROR,
-									"Error in main while retrieve the connection for database "
-											+ myDBConn[i].getName() + " error:  " + e);
-						}
-					} else { // if database is become unreachable i'll send
-								// noDataFound
-						BlockingQueue<ZabbixItem> _queue = new LinkedBlockingQueue<ZabbixItem>();
-						for (int cnt = 0; cnt < q.length; cnt++) {
-							_queue.offer(new ZabbixItem(q[cnt].getName(),
-									q[cnt].getNoData()));
-						}
-						Sender sender = new Sender(_queue, zabbixServers,
-								myDBConn[i].getName());
-						sender.run();
-					}
+					Configurator.logThis(Level.DEBUG,
+							"Ready to run DBJob for dbname ->"
+									+ myDBConn[i].getName());
+					Runnable runner = new DBJob(spds, q,
+							Constants.QUERY_LIST, zabbixServers,
+							myDBConn[i].getName());
+					executor.execute(runner);
+				
 				}
-
-				//Configurator.logThis(Level.DEBUG,"going in bed...and sleep for "+ Configurator.getSleep() * 1000 + " ms");
-								
-				// System.out.println("going in bed...and sleep for "+c.getSleep()*1000/60000+
-				// " m");
-				//Thread.sleep(Configurator.getSleep() * 1000);
 				Thread.sleep(60*1000);
 				Configurator.logThis(Level.DEBUG, "Waking up Goood Morning");
 
