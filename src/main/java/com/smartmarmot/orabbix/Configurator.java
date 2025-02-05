f23988ae3ae755dd83cb86a6ec83893e2682c365
snickerjp <snickerjp@gmail.com>
2025-02-05 15:12:15 +0000

    update checkstyle version and configuration; add maven assembly plugin

--- src/main/java/com/smartmarmot/orabbix/Configurator.java
+++ src/main/java/com/smartmarmot/orabbix/Configurator.java
@@ -23,6 +23,7 @@ package com.smartmarmot.orabbix;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
